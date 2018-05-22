261debb497c5d2a5773a44fbf5c483c4088a5b9a
Jungwoo Song <thdwjddn999@gmail.com>
2018-05-22 21:45:23 +0900

    Fix SimpleMinMap class extends JFrame

--- src/MindMap/SimpleMindMap.java
+++ src/MindMap/SimpleMindMap.java
@@ -7,9 +7,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
-public class SimpleMindMap {
+public class SimpleMindMap extends JFrame{
+	TopEvent topEvent;
+	TopMenuBar topMenuBar;
+	TopToolBar topToolBar;
+	Container contentPane;
+	JSplitPane mainPane;
+	JPanel textEditorPane;
+	JPanel mindMapPane;
+	JPanel attributePane;
+	public SimpleMindMap() {
+		super("SimpleMindMap");
+		init();
+	}
 
 	public static void main(String[] args) {
+		SimpleMindMap simpleMindMap = new SimpleMindMap();
+		/*
 		JFrame window = new JFrame("SimpleMindMap");
 		TopEvent topEvent = new TopEvent();
 		TopMenuBar topMenuBar = new TopMenuBar();
@@ -42,7 +56,36 @@ public class SimpleMindMap {
 		mainPane.setRightComponent(attributePane);
 		mainPane.setLeftComponent(textEditorPane);
 		mainPane.setDividerLocation((int)window.getSize().getWidth()/2);
-	
+		*/
 	}
 
+	private void init() {
+		topEvent = new TopEvent();
+		topMenuBar = new TopMenuBar();
+		topToolBar = new TopToolBar();
+		contentPane = getContentPane();
+		mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		textEditorPane = new JPanel();  
+		mindMapPane = new JPanel();  
+		attributePane = new JPanel();  
+
+		topMenuBar.attachTopEvent(topEvent);
+		topToolBar.attachTopEvent(topEvent);
+
+        textEditorPane.setBackground(Color.BLACK);
+        mindMapPane.setBackground(Color.GRAY);
+        attributePane.setBackground(Color.WHITE);
+
+		contentPane.add(topToolBar, BorderLayout.NORTH);
+        contentPane.add(mainPane, BorderLayout.CENTER);  
+        
+        mainPane.setContinuousLayout(true);
+		mainPane.setRightComponent(attributePane);
+		mainPane.setLeftComponent(textEditorPane);
+		mainPane.setDividerLocation((int)this.getSize().getWidth()/2);
+
+		setJMenuBar(topMenuBar);
+		setSize(400, 400);
+		setVisible(true);
+	}
 }
